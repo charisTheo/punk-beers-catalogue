fdffbec4c6fb5d85cdb0d98c2c620f7f3cee79ce
charisTheo <haristh@hotmail.com>
2019-07-07 12:16:45 +0100

    Updates

---
+++ precache-manifest.831ce2fe6ec8540b37f0174f27ab273e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c034277792bf1e0ebeb545f994c0188b",
+    "url": "/punk-beers-catalogue/index.html"
+  },
+  {
+    "revision": "78dfb4376192aaa91144",
+    "url": "/punk-beers-catalogue/static/css/main.c4db1b56.chunk.css"
+  },
+  {
+    "revision": "f1a2c9eb21e601cc6fa0",
+    "url": "/punk-beers-catalogue/static/js/2.83eb5c95.chunk.js"
+  },
+  {
+    "revision": "78dfb4376192aaa91144",
+    "url": "/punk-beers-catalogue/static/js/main.1d3e3d33.chunk.js"
+  },
+  {
+    "revision": "a9bafeb44cdd1ad2370d",
+    "url": "/punk-beers-catalogue/static/js/runtime~main.d9b0e77b.js"
+  }
+]);
